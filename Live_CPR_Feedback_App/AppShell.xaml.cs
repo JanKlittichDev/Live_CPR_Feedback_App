301de5286b7920d588a2781044c8ccfee2c84804
Jan Klittich <jan.volker.klittich@gmail.com>
2026-07-14 14:31:46 +0200

    Projektdateien hinzufügen.

---
+++ Live_CPR_Feedback_App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Live_CPR_Feedback_App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Live_CPR_Feedback_App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Live_CPR_Feedback_App
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
